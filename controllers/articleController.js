3e3e692df64bb35c788697a3da05c62d2c2c0b01
ITTECH10 <zivinice.connected@gmail.com>
2021-11-23 11:46:09 +0100

    get all articles

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -16,3 +16,16 @@ exports.createArticle = catchAsync(async (req, res, next) => {
         article
     })
 })
+
+exports.getAllArticles = catchAsync(async (req, res, next) => {
+    const articles = await Article.find()
+
+    if (!articles) {
+        return next(new AppError('No articles found'))
+    }
+
+    res.status(200).json({
+        message: 'success',
+        articles
+    })
+})
--- routers/articleRouter.js
+++ routers/articleRouter.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const authController = require('./../controllers/authController')
+// const authController = require('./../controllers/authController')
 const articleController = require('./../controllers/articleController')
 
 const router = express.Router()
 
 // BELLOW ROUTES ARE PROTECTED
-router.use(authController.protect)
+// router.use(authController.protect)
 
 router.route('/')
     .post(articleController.createArticle)
+    .get(articleController.getAllArticles)
 
 module.exports = router
